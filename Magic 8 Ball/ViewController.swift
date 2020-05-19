38aeba3da9098246dd97b0f5bc7cbe4924864f67
Fabio <soaresfabiojr@gmail.com>
2020-05-19 18:19:47 +1200

    create IBOutlets and IBAction on view controller

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var personImage: UIImageView!
+    @IBOutlet weak var ballImage: UIImageView!
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        ballImage.image = ballArray.randomElement()
+    }
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
